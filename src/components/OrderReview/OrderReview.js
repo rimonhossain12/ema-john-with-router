6a3c03cb31b98a542ce3b171583e7c15822fb465
rimonspi12 <rimonmpi@gmail.com>
2021-10-01 22:49:53 +0600

    show data in order review page

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
 
 
 const OrderReview = () => {
     const [products] = useProducts();
     const [cart] = useCart(products);
     return (
-        <div>
-            <h1>total products length is : {products.length}</h1>
-            <h3>{cart.length}</h3>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
