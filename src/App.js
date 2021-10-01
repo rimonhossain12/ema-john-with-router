4fb94d2bccdf34c8cfab307c2e8662c74f243f71
rimonspi12 <rimonmpi@gmail.com>
2021-10-01 21:43:00 +0600

    router is added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
+import Inventory from './Inventory/Inventory';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
-
+          <Router>
+              <Switch>
+                  <Route exact path="/">
+                      <Shop></Shop>
+                  </Route>
+                  <Route path="/shop">
+                      <Shop></Shop>
+                  </Route>
+                  <Route path="/orders">
+                      <OrderReview></OrderReview>
+                  </Route>
+                  <Route path="/inventory">
+                      <Inventory></Inventory>
+                  </Route>
+                  <Route path="*">
+                      <NotFound></NotFound>
+                  </Route>
+              </Switch>
+          </Router>
     </div>
   );
 }
